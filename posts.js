ed7d953e0660097414ddd299776e5e8df72d35b2
kasia.kordys <kasia.kordys@gmail.com>
2024-10-07 23:06:28 +0300

    add logger middleware

--- posts.js
+++ posts.js
@@ -1,3 +1,6 @@
+const { createStore, applyMiddleware } = require("redux");
+const loggerMiddleware = require("redux-logger").createLogger();
+
 // initial state
 
 const initialState = {
@@ -6,32 +9,44 @@ const initialState = {
 // actions
 
 const fetchPostsRequest = () => {
-    return {
-        type: "FETCH_DISPATCHED";
-    }
- 
+  return {
+    type: "FETCH_DISPATCHED",
+  };
 };
 
 const fetchPostsSuccess = () => {
-    return {
-        type: "FETCH_SUCCESS"
-    };
-}
- 
+  return {
+    type: "FETCH_SUCCESS",
+  };
+};
 
 const fetchPostsFailed = () => {
-    return {
-
-        type: "FETCH_FAILED"
-    }
+  return {
+    type: "FETCH_FAILED",
+  };
 };
 
 // reducer
 
-const postReducer = (state = initialState, action) => {}
+const postReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_DISPATCHED":
+      return {
+        posts: { name: "Lucy" },
+      };
+  }
+};
 
 // store
 
-const store = createStore(postReducer);
+const store = createStore(postReducer, applyMiddleware(loggerMiddleware));
 
+// subscribe
+
+store.subscribe(() => {
+  const data = store.getState();
+  console.log("data", data.posts);
+});
 // dispatch actions
+
+store.dispatch(fetchPostsRequest());
