deb1c6fe427d18d1efa6a4549c8d47d127fe21d7
kasia.kordys <kasia.kordys@gmail.com>
2024-10-09 20:36:06 +0300

    add action creator and update reducer

--- posts.js
+++ posts.js
@@ -1,5 +1,6 @@
 const { createStore, applyMiddleware } = require("redux");
-
+const axios = require("axios");
+const { thunk } = require("redux-thunk");
 
 // initial state
 
@@ -11,39 +12,61 @@ const initialState = {
 
 // custom middleware
 
-const customLogger = () => {
-  return (next) => {
-    return (action) => {
-      console.log("custom logger", action);
-    };
-  };
-  next(action);
-};
+// const customLogger = () => {
+//   return (next) => {
+//     return (action) => {
+//       console.log("custom logger", action);
+//     };
+//   };
+//   next(action);
+// };
 
 // actions constants
 
 const REQUEST_STARTED = "REQUEST_STARTED";
 const FETCH_SUCCESS = "FETCH_SUCCESS";
-const FETCH_FAILED = "REQUEST_FAILED";
+const FETCH_FAILED = "FETCH_FAILED";
 
 // actions
 
 const fetchPostsRequest = () => {
   return {
     type: REQUEST_STARTED,
-    payload: { name: "Xara" },
   };
 };
 
-const fetchPostsSuccess = () => {
+const fetchPostsSuccess = (posts) => {
   return {
     type: FETCH_SUCCESS,
+    payload: posts,
   };
 };
 
-const fetchPostsFailed = () => {
+const fetchPostsFailed = (error) => {
   return {
     type: FETCH_FAILED,
+    payload: error,
+  };
+};
+
+// action creator from thunk, returns function with dispatch a an argument
+
+const fetchPosts = () => {
+  return async (dispatch) => {
+    try {
+      // dispatch fetch
+      dispatch(fetchPostsRequest());
+      const data = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts/test"
+      );
+
+      // data back
+      // dispatch success
+      dispatch(fetchPostsSuccess(data));
+    } catch (error) {
+      // dispatch error
+      dispatch(fetchPostsFailed(error.message));
+    }
   };
 };
 
@@ -53,21 +76,34 @@ const postReducer = (state = initialState, action) => {
   switch (action.type) {
     case REQUEST_STARTED:
       return {
-        posts: { name: "Lucy" },
+        ...state,
+        loading: true,
+      };
+    case FETCH_SUCCESS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false,
+      };
+    case FETCH_FAILED:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
       };
   }
 };
 
 // store
 
-const store = createStore(postReducer, applyMiddleware(customLogger));
+const store = createStore(postReducer, applyMiddleware(thunk));
 
 // subscribe
 
 store.subscribe(() => {
   const data = store.getState();
-  console.log("data", data.posts);
+  console.log("data", data);
 });
 // dispatch actions
 
-store.dispatch(fetchPostsRequest());
+store.dispatch(fetchPosts());
